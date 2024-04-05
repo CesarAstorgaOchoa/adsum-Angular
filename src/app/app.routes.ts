1788023cf2fbe9f5f865cc5afbb88b4e72a9d6d7
CesarAstorgaOchoa <18170276@itculiacan.edu.mx>
2024-04-04 22:41:39 -0700

    primer commit, componente captura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CapturaComponent } from './pages/captura/captura.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: CapturaComponent
+    }
+
+];
---
+++ src/app/pages/captura/captura.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-captura',
+  standalone: true,
+  imports: [],
+  templateUrl: './captura.component.html',
+  styleUrl: './captura.component.css'
+})
+export class CapturaComponent {
+
+}
